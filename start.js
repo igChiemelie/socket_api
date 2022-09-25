1aac93527d62fe396cdc28c76a5429f8543a3a19
igChiemelie <79201826+igChiemelie@users.noreply.github.com>
2022-09-25 15:47:55 -0700

    Update start.js

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
+const http = require('https').Server(app);
 
 const socket = require("socket.io");
 
